51acd94cac9a38d1a46636d880607b6665e0372c
Michael Andersen <michael.andersen.85@gmail.com>
2018-11-13 14:15:45 +0100

    Cleanup test file

--- test/aion_cli_test.rb
+++ test/aion_cli_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
-class AionCliTest < Minitest::Test
+class AionCLITest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::AionCli::VERSION
+    refute_nil ::AionCLI::VERSION
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
   end
 end
