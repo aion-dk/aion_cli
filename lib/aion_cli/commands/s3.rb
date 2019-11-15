b9613d29e9f99b1e3bb01cf7d54b4a3f232dc618
afibsen <alexander@aion.dk>
2019-11-15 15:28:18 +0100

    Shitfix for thomas

--- lib/aion_cli/commands/s3.rb
+++ lib/aion_cli/commands/s3.rb
@@ -1,44 +1,44 @@
-require 'aion-s3'
-
-module AionCLI
-  module CLI
-    class S3 < Thor
-      include AionCLI::ApplicationHelper
-
-      desc 'unpack PATH', 'Decrypts and decompresses a file'
-      def unpack(path)
-        data = File.read(path, mode: 'rb')
-
-        password = ask("Password:")
-        packer = AionS3::Packer.new(password)
-
-        output_path = ask_output_path
-        File.open(output_path, 'wb') do |io|
-          io.write(packer.unpack(data))
-        end
-        say("Done! Output stored in #{output_path}", :green)
-      end
-
-      desc 'pack PATH', 'Compresses and encrypts a file'
-      def pack(path)
-        data = File.read(path, mode: 'rb')
-
-        password = ask("Password [leave blank to generate random]:")
-
-        if password.present?
-          packer = AionS3::Packer.new(password)
-        else
-          packer = AionS3::Packer.new
-          say("Password set to #{packer.password}")
-        end
-
-        output_path = ask_output_path
-        File.open(output_path, 'wb') do |io|
-          io.write(packer.pack(data))
-        end
-        say("Done! Output stored in #{output_path}", :green)
-      end
-
-    end
-  end
-end
+# require 'aion-s3'
+#
+# module AionCLI
+#   module CLI
+#     class S3 < Thor
+#       include AionCLI::ApplicationHelper
+#
+#       desc 'unpack PATH', 'Decrypts and decompresses a file'
+#       def unpack(path)
+#         data = File.read(path, mode: 'rb')
+#
+#         password = ask("Password:")
+#         packer = AionS3::Packer.new(password)
+#
+#         output_path = ask_output_path
+#         File.open(output_path, 'wb') do |io|
+#           io.write(packer.unpack(data))
+#         end
+#         say("Done! Output stored in #{output_path}", :green)
+#       end
+#
+#       desc 'pack PATH', 'Compresses and encrypts a file'
+#       def pack(path)
+#         data = File.read(path, mode: 'rb')
+#
+#         password = ask("Password [leave blank to generate random]:")
+#
+#         if password.present?
+#           packer = AionS3::Packer.new(password)
+#         else
+#           packer = AionS3::Packer.new
+#           say("Password set to #{packer.password}")
+#         end
+#
+#         output_path = ask_output_path
+#         File.open(output_path, 'wb') do |io|
+#           io.write(packer.pack(data))
+#         end
+#         say("Done! Output stored in #{output_path}", :green)
+#       end
+#
+#     end
+#   end
+# end
